02da2f147359668370d697441fc1d05ec6247a97
Vincent Doucette <vincent.doucette@bhcc.edu>
2025-12-13 23:49:35 -0500

    Update automatetheboringstuff.py
    corrected choice logic after a couple attempts
    corrected how answers are accepted; would ask again if I used proper capitalization

--- Documents/csc125/automatetheboringstuff.py
+++ Documents/csc125/automatetheboringstuff.py
@@ -2,13 +2,13 @@ import random
 guess = ''
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
-    guess = input()
-toss = random.(('heads', 'tails'))
+    guess = input().lower()
+toss = random.choice(('heads', 'tails'))
 if toss == guess:
     print('You got it!')
 else:
     print('Nope! Guess again!')
-    guess = input()
+    guess = input().lower()
     if toss == guess:
        print('You got it!')
     else:
