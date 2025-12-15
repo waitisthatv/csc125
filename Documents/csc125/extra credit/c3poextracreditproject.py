26a32b65776f52abae0c8dccbf4f76554fcd2365
Vincent Doucette <vincent.doucette@bhcc.edu>
2025-12-15 16:46:01 -0500

    Create c3poextracreditproject.py
    created form of pseudocode to make my life easier

---
+++ Documents/csc125/extra credit/c3poextracreditproject.py
@@ -0,0 +1,8 @@
+# if we say r2d2 in sentence = “R2D2, he should know better than to trust a strange computer!”
+# if we say millennium falcon in sentence = “Sir, the possibility of successfully navigating an asteroid field is approximately 3,720 to 1.”  
+# if sentence ends with ? = "For a mechanic, you seem to do an incessant amount of thinking <name>. If I told you half the things I've heard about this Jabba the Hutt, you'd probably short circuit.” 
+# if sentence ends with ! = “Sometimes I just don’t understand human behavior, <name>! After all, I’m only trying to do my job. What else?”
+# if sentence starts with I feel = “When I feel that way…well droids don’t feel. Master <name>, it’s why nobody worries about upsetting a droid.” 
+# if line says I am <something> = “When I was last <something> I suggested a new strategy to R2: to let the Wookiee win.” C3PO will replace the <something> with what the user typed in.
+# if you say go away once = “Hang on tight, Master <name>. You’ve got to come back. You wouldn’t want my life to get boring, would you?” 
+# Create a function to calculate the total number of letters that the user has inputted.  So that when C3PO hears “go away” for the second time, C3PO says “Master <name>, go that way! You have used <number of letters> in our chat today. You’ll be malfunctioning within a day, you nearsighted scrap pile. And don’t let me catch you following me, begging for help, because you won’t get it!” Be sure to replace <number of letters> with the calculated number of letters used by the user. 
